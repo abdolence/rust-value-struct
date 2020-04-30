a097f4309108a9ad1c32bfbd289d83ddca92e214
Abdulla Abdurakhmanov <abdulla.abd.m@gmail.com>
2020-04-30 12:16:16 +0300

    Unit tests updates

--- rvs_test/src/lib.rs
+++ rvs_test/src/lib.rs
@@ -14,28 +14,25 @@ mod tests {
 
     #[test]
     fn create_str_value_struct() {
-        let s1 = SimpleStrValueStruct::from(String::from("Hey"));
+        let s1 = String::from("Hey").into();
         assert_eq!(s1.value(), "Hey");
 
-        let s12 = SimpleStrValueStruct::from("Hey");
+        let s12 = "Hey".into();
         assert_eq!(s12.value(), "Hey");
 
         let s13 = SimpleStrValueStruct::from(s12);
         assert_eq!(s13.value(), "Hey");
-
-        let s14 : SimpleStrValueStruct = "Hey".into();
-        assert_eq!(s14.value(), "Hey");
     }
 
     #[test]
     fn create_std_str_value_struct() {
-        let s1 = StdStrValueStruct::from(String::from("Hey"));
+        let s1 = String::from("Hey").into();
         assert_eq!(s1.value(), "Hey");
     }
 
     #[test]
     fn create_int_value_struct() {
-        let i1 = SimpleIntValueStruct::from(1u8);
+        let i1 = 1u8.into();
         assert_eq!(i1.value(), 1u8);
     }
 }
